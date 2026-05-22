ea9d7dea9b0df4faaaa712aa8f8f24fc74c2f350
Radomyr Klymov <r.klymov@jobware.de>
2026-05-22 11:21:39 +0200

    updated db and repos

--- app/src/main/java/com/organizer/data/local/db/AppDatabase.kt
+++ app/src/main/java/com/organizer/data/local/db/AppDatabase.kt
@@ -4,18 +4,26 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.organizer.data.local.dao.CategoryDao
 import com.organizer.data.local.dao.ExerciseDao
+import com.organizer.data.local.dao.WorkoutDao
+import com.organizer.data.local.dao.WorkoutExerciseDao
 import com.organizer.data.local.db.entities.CategoryEntity
 import com.organizer.data.local.db.entities.ExerciseEntity
+import com.organizer.data.local.db.entities.WorkoutEntity
+import com.organizer.data.local.db.entities.WorkoutExerciseEntity
 
 @Database(
     version = 1,
     entities = [
         CategoryEntity::class,
         ExerciseEntity::class,
+        WorkoutEntity::class,
+        WorkoutExerciseEntity::class
     ],
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract val categoryDao: CategoryDao
     abstract val exerciseDao: ExerciseDao
+    abstract val workoutDao: WorkoutDao
+    abstract val workoutExerciseDao: WorkoutExerciseDao
 }
---
+++ app/src/main/java/com/organizer/data/repo/WorkoutExerciseRepository.kt
@@ -0,0 +1,22 @@
+package com.organizer.data.repo
+
+import com.organizer.data.local.dao.WorkoutExerciseDao
+import com.organizer.data.local.db.entities.WorkoutExerciseEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class WorkoutExerciseRepository @Inject constructor(
+    private val dao: WorkoutExerciseDao
+) {
+    suspend fun addExerciseToWorkout(workoutId: Long, exerciseId: Long) {
+        dao.insert(WorkoutExerciseEntity(workoutId, exerciseId))
+    }
+
+    fun observeWorkoutExercises(): Flow<List<WorkoutExerciseEntity>> {
+        return dao.getAll()
+    }
+
+    suspend fun deleteWorkout(workoutId: Long, exerciseId: Long) {
+        dao.delete(WorkoutExerciseEntity(workoutId, exerciseId))
+    }
+}
