705b7c6bdb805ac90a2668987d22619ba180389d
Radomyr Klymov <r.klymov@jobware.de>
2026-08-03 11:14:06 +0200

    using new api url from railway deploy

--- app/src/main/java/com/organizer/constants/AppConfig.kt
+++ app/src/main/java/com/organizer/constants/AppConfig.kt
@@ -2,5 +2,5 @@ package com.organizer.constants
 
 object AppConfig {
     const val DATABASE_NAME = "app_db"
-    const val API_BASE_URL = "https://exercise-organizer.onrender.com"
+    const val API_BASE_URL = "https://exerciseapi-production-50be.up.railway.app"
 }
